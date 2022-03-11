7e9a2a3fb253012813191cb61d8cf81c863cdf23
smarttommyau <smarttommyau@gmail.com>
2022-03-11 23:32:47 +0800

    some updates

--- linux/client.cpp
+++ linux/client.cpp
@@ -4,6 +4,13 @@
 #include"lib/stdc++.h"
 //program lib
 #include "lib/text-app.h"
+#include "cryptopp/eccrypto.h"
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/oids.h"
+#include "cryptopp/hex.h"
+#include "cryptopp/filters.h"
+std::string encrypt_ecc(std::string text,std::string pubkey);
 int main(int argc,char const *argv[])
 {
 	struct send_data send_data;
@@ -14,11 +21,36 @@ int main(int argc,char const *argv[])
 	std::stringstream p;
 	p << argv[1];
 	p >> send_data.port;
+	if(argc > 4){
+		std::cout << "en" << std::endl;
+		send_data.message = encrypt_ecc(send_data.message,argv[4]);
+	}
 	//send pack
+	
 	std::string return_msg = send_pack(send_data);
-	if(return_msg.find("err") != std::string::npos)
-		std::cout << "err";
-	else
-		std::cout << return_msg;
+
 	return 0;
+}
+std::string encrypt_ecc(std::string text,std::string pubkey){
+    //basic variable
+    std::string cipher;
+    try{
+    CryptoPP::StringSource key_s(pubkey,true, new CryptoPP::HexDecoder);
+    CryptoPP::ECIES<CryptoPP::ECP>::Encryptor en(key_s);
+    
+
+    size_t cipher_text_size = en.CiphertextLength(text.size());
+    cipher.resize(cipher_text_size);
+    CryptoPP::RandomPool rnd;
+    en.Encrypt(rnd,(CryptoPP::byte*)text.c_str(),text.size(),(CryptoPP::byte*)cipher.data());
+	std::string output;
+	std::cout << cipher << std::endl;
+	CryptoPP::HexEncoder hexen(new CryptoPP::StringSink(output));
+	hexen.Put((CryptoPP::byte*)cipher.data(),cipher.length());
+	hexen.MessageEnd();
+    return output;
+}catch(const CryptoPP::Exception& e){
+    std::cerr << e.what() << std::endl;
+        return "";
+}    
 }
--- linux/lib/network.h
+++ linux/lib/network.h
@@ -5,17 +5,37 @@
 //network lib
 #include<sys/socket.h>
 #include<arpa/inet.h>
+#include<sys/ioctl.h>
 struct send_data{
 	std::string message;
 	std::string serverip;
 	int port;
 };
+std::string getmessage(int new_socket){
+	int count,precount;
+	ioctl(new_socket,FIONREAD,&count);
+	precount = -1;
+	int slow = 0;
+	do{
+			usleep(100);
+			precount = count;
+			ioctl(new_socket,FIONREAD,&count);					
+	}while(precount != count&&count != 0);
+	if(count != 0){
+		char buffer[count];
+		read(new_socket,&buffer,count);    
+		std::string string = buffer;
+		return string;
+	}else{
+		return "";
+	}
+}
 std::string send_pack(struct send_data send_data){
 	//var for network
 	int sock = 0,valread,port;
 	struct sockaddr_in serv_addr;
 	//main
-	char buffer[1024] ={0};
+	char buffer[2048] ={0};
 	//create socket file
 	sock = socket(AF_INET,SOCK_STREAM, 0);
 	//valid if socket file create successfully
@@ -31,10 +51,18 @@ std::string send_pack(struct send_data send_data){
 	if(connect(sock, (struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
 		return "err3:connection failed";
 	//send message
+	long unsigned int len = send_data.message.length();
+	
+	send(sock,(void*)&len,sizeof(len),0);
+	std::cout << "len:" << len << "bytes" << std::endl;
 	send(sock,send_data.message.c_str(),send_data.message.length(),0);
+	std::cout << "raw:" << send_data.message.c_str() << std::endl;
 	std::cout << "message sent" << std::endl;
-	//read and return message
-	valread = read(sock,buffer,1024);
-	std::string strbuffer = buffer;
-	return strbuffer;
+	//read and return message	
+		//std::string strbuffer = getmessage(sock);
+		read(sock,&buffer,2048); 
+		std::string strbuffer = buffer;
+		std::cout << strbuffer << std::endl;
+
+	return "";
 }
