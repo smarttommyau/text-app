8cf40dc405677eaff19ad644173970573d8fcd17
smarttommyau <smarttommyau@gmail.com>
2022-03-12 16:20:45 +0800

    clean up

--- linux/lib/network.h
+++ linux/lib/network.h
@@ -6,29 +6,61 @@
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<sys/ioctl.h>
+#define CLOCKS_PER_MILLISECONDS CLOCKS_PER_SEC / 1000
+#define CLOCKS_PER_NANOSECONDS CLOCKS_PER_MILLISECONDS/1000
 struct send_data{
 	std::string message;
 	std::string serverip;
 	int port;
 };
+
+bool readbits(int socket,void* buffer,unsigned int size){
+	int finished = 0;
+	int result;
+	long long run;
+	clock_t end = clock() + CLOCKS_PER_SEC;
+	while(finished < size&&clock() < end){
+		int count = 0; 
+		result = 0;
+		ioctl(socket,FIONREAD,&count);
+		if(count > 0){
+			result = read(socket, buffer + finished, size - finished);
+			finished += result;
+			std::cout << "read:" << finished << "/" << size << "bytes" << std::endl;
+			std::cout << "loop:" << run << "(" <<(float)(clock()- (end - CLOCKS_PER_SEC))/(CLOCKS_PER_NANOSECONDS) << "ns)" << std::endl;
+			end = clock() + CLOCKS_PER_SEC;
+		}
+		
+		run++;
+	}
+	
+	if(finished < size){
+		std::cout << "timeout" << std::endl;
+		return false;
+	}
+	return true;
+}
 std::string getmessage(int new_socket){
-	int count,precount;
-	ioctl(new_socket,FIONREAD,&count);
-	precount = -1;
-	int slow = 0;
-	do{
-			usleep(100);
-			precount = count;
-			ioctl(new_socket,FIONREAD,&count);					
-	}while(precount != count&&count != 0);
-	if(count != 0){
-		char buffer[count];
-		read(new_socket,&buffer,count);    
-		std::string string = buffer;
-		return string;
-	}else{
+	std::string string;
+	//get length
+	long unsigned int len = 0;
+	char* buffer = 0;
+	if(!readbits(new_socket,(void*)(&len),sizeof(len))){
 		return "";
 	}
+	std::cout << "socket:" << len << "bytes" << std::endl;
+	buffer = new char[len+1];
+	if(!readbits(new_socket,(void*)buffer,len)){
+		return "dead";
+	}
+	buffer[len] = '\0';
+
+	string = buffer;
+	delete[] buffer;
+	
+	return string;
+	
+
 }
 std::string send_pack(struct send_data send_data){
 	//var for network
@@ -59,9 +91,7 @@ std::string send_pack(struct send_data send_data){
 	std::cout << "raw:" << send_data.message.c_str() << std::endl;
 	std::cout << "message sent" << std::endl;
 	//read and return message	
-		//std::string strbuffer = getmessage(sock);
-		read(sock,&buffer,2048); 
-		std::string strbuffer = buffer;
+		std::string strbuffer = getmessage(sock);
 		std::cout << strbuffer << std::endl;
 
 	return "";
